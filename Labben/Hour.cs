a66ef8f98be4765255c1ca59e304b19f1d52e79a
Rasmus Ekbom <rasmus.ekbom@edu.edugrade.se>
2021-11-04 11:42:45 +0100

    start

---
+++ Labben/Hour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labben
+{
+    class Hour : Form
+    {
+    //    public int hourinput = 0;    
+    //    public void HourInput(Form1 textBox1, Form1 label7)
+    //{
+    //        Int32.TryParse(textBox1.Text, out hourinput);
+    //        if (hourinput < 0)
+    //        {
+    //            textBox1.Text = "0";
+    //            label7.Text = "!!  Minimum allowed number is 0  !!";
+    //        }
+    //        else if (hourinput >= 24)
+    //        {
+    //            textBox1.Text = "23";
+    //            label7.Text = "!!  Maximum allowed number is 23  !!";
+    //        }
+    //    }
+
+    }
+}
